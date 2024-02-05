00eeed357380ecd3e47443ceb125a9e23c6d7425
Thiago Campos <thiago-kono@hotmail.com>
2024-02-05 17:58:26 -0300

    add

--- test2.py
+++ test2.py
@@ -1,30 +1,41 @@
+import random
 import time
 import sys
 from termcolor import colored
 import msvcrt
 
-
-
 def display_text_with_highlight(input_text, current_index, user_input):
     display_text = ""
     for i, char in enumerate(input_text):
         if i < current_index:
-            display_text += char
+            display_text += colored(char, 'grey')  # Original text color in light grey
         elif i == current_index:
             if user_input and user_input[-1] == char:
                 display_text += colored(char, 'green')
             else:
                 display_text += colored(char, 'white', 'on_red')  # Background color red for incorrect input
         else:
-            display_text += ' '
+            display_text += colored(char, 'grey')  # Original text color in light grey
 
     sys.stdout.write("\r" + display_text)
     sys.stdout.flush()
 
+def countdown():
+    for i in range(3, 0, -1):
+        sys.stdout.write(f"\r{i}")
+        sys.stdout.flush()
+        time.sleep(1)
+    sys.stdout.write("\r   \r")
+    sys.stdout.flush()
+
 def test_speed():
-    input_text = "Lorem i"
-    print(input_text)
-    print()
+    input_texts = ["Lorem 1", "Lorem 2", "Lorem 3", "Lorem 4"]
+    input_text = random.choice(input_texts)
+
+    print("Preparar-se para o teste:")
+    countdown()
+
+    print("\n" + input_text + "\n")
 
     user_input = ""
     current_index = 0
